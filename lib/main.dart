1cb0cae00073c1954022b9ac4723c366e530083f
Fahadsattar744 <fahadfahad744@gmail.com>
2024-09-05 15:44:27 +0500

    firebase integrated, web,andriod and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(MyApp());
 }
 
